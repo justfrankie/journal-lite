0e0d13e854c93748cde17b9642c0970cb3dac4ce
Frankie <zhuangfrankie@gmail.com>
2021-03-02 00:17:12 -0800

    prettier on code

--- database.js
+++ database.js
@@ -1,61 +1,61 @@
-const mysql = require('mysql')
+const mysql = require("mysql");
 
 let connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'journallite'
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "",
 });
 
-connection.connect(function(err) {
-    if (err) {
-      return console.error('error: ' + err.message);
-    }
-  
-    console.log('Connected to the MySQL server.');
-  });
+connection.connect(function (err) {
+  if (err) {
+    return console.error("error: " + err.message);
+  }
 
-let helpers =  {
-    getAll: (callback) => {
-      let queryString = `SELECT * FROM journals`;
-      connection.query(queryString, (err, result) => {
-        if (err) {
-          callback(err);
-        } else {
-          callback(null, result)
-        }
-      })
-    },
-    postEntry: (data, callback) => {
-        let queryString = `INSERT INTO journals (title, date, body) VALUES ("${data.title}", "${data.date}", "${data.body}");`
-        connection.query(queryString, (err, result) => {
-            if (err) {
-              callback(err);
-            } else {
-              callback(null, result)
-            }
-        })
-    }, 
-    updateOne: (id, data, callback) => {
-        let queryString = `UPDATE journals SET title="${data.title}", body="${data.body}" WHERE id =${id};`
-        connection.query(queryString, (err, result) => {
-            if (err) {
-              callback(err);
-            } else {
-              callback(null, result)
-            }
-        })
-    },
-    getOne: (id, callback) => {
-        let queryString = `SELECT * FROM journals WHERE id =${id};`
-        connection.query(queryString, (err, result) => {
-            if (err) {
-              callback(err);
-            } else {
-              callback(null, result)
-            }
-        })
-    }
-}
+  console.log("Connected to the MySQL server.");
+});
 
-  module.exports = helpers
+let helpers = {
+  getAll: (callback) => {
+    let queryString = `SELECT * FROM journals`;
+    connection.query(queryString, (err, result) => {
+      if (err) {
+        callback(err);
+      } else {
+        callback(null, result);
+      }
+    });
+  },
+  postEntry: (data, callback) => {
+    let queryString = `INSERT INTO journals (title, date, body) VALUES ("${data.title}", "${data.date}", "${data.body}");`;
+    connection.query(queryString, (err, result) => {
+      if (err) {
+        callback(err);
+      } else {
+        callback(null, result);
+      }
+    });
+  },
+  updateOne: (id, data, callback) => {
+    let queryString = `UPDATE journals SET title="${data.title}", body="${data.body}" WHERE id =${id};`;
+    connection.query(queryString, (err, result) => {
+      if (err) {
+        callback(err);
+      } else {
+        callback(null, result);
+      }
+    });
+  },
+  getOne: (id, callback) => {
+    let queryString = `SELECT * FROM journals WHERE id =${id};`;
+    connection.query(queryString, (err, result) => {
+      if (err) {
+        callback(err);
+      } else {
+        callback(null, result);
+      }
+    });
+  },
+};
+
+module.exports = helpers;
--- server.js
+++ server.js
@@ -1,60 +1,61 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const morgan = require('morgan');
-const cors = require('cors');
-const path = require('path');
-const db = require('./database.js');
+const express = require("express");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const cors = require("cors");
+const path = require("path");
+const db = require("./database.js");
 
 const app = express();
 const PORT = 8000;
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-app.use('/', express.static(path.join(__dirname, './client/dist')));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use("/", express.static(path.join(__dirname, "./client/dist")));
 app.use(cors());
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
 // REST -> get, post, put, delete
-// also db.helpers <- what are we exporting from the database file 
-app.get('/all', (req, res) => {
-  db.getAll((err, results) => { 
+// also db.helpers <- what are we exporting from the database file
+app.get("/all", (req, res) => {
+  db.getAll((err, results) => {
     if (err) {
-      res.status(404).send(err)
+      res.status(404).send(err);
     } else {
-      res.status(200).send(results)
+      res.status(200).send(results);
     }
-  })
-})
+  });
+});
 
-app.post('/post', (req, res) => {
-    db.postEntry( req.body ,(err, results) => { 
-      if (err) {
-        res.status(404).send(err)
-      } else {
-        res.status(200).send("alvin has posted!!!!!!!!!")
-      }
-    })
-  })
+app.post("/post", (req, res) => {
+  db.postEntry(req.body, (err, results) => {
+    if (err) {
+      res.status(404).send(err);
+    } else {
+      res.status(200).send("alvin has posted!!!!!!!!!");
+    }
+  });
+});
 
-  app.put('/update/:id', (req, res) => {
-    db.updateOne( req.params.id , req.body ,(err, results) => { 
-      if (err) {
-        res.status(404).send(err)
-      } else {
-        res.status(200).send("alvin has updated!!!!!!")
-      }
-    })
-  })
+app.put("/update/:id", (req, res) => {
+  db.updateOne(req.params.id, req.body, (err, results) => {
+    if (err) {
+      res.status(404).send(err);
+    } else {
+      res.status(200).send("alvin has updated!!!!!!");
+    }
+  });
+});
 
-  app.get('/get/:id', (req, res) => {
-    db.getOne( req.params.id ,(err, results) => { 
-      if (err) {
-        res.status(404).send(err)
-      } else {
-        res.status(200).send(results)
-      }
-    })
-  })
+app.get("/get/:id", (req, res) => {
+  db.getOne(req.params.id, (err, results) => {
+    if (err) {
+      res.status(404).send(err);
+    } else {
+      res.status(200).send(results);
+    }
+  });
+});
 
-app.listen(PORT, () => {`LISTENING ON PORT ${PORT}`});
-
+app.listen(PORT, () => {
+  `LISTENING ON PORT ${PORT}`;
+});
